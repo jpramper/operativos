7f6f064d8bdc6b40e38cee380e2b34d9635ebbd2
CuRy <curyman@gmail.com>
2014-09-23 20:33:34 -0500

    clone and pthread versions has been added

---
+++ practica3/borders_pthread.c
@@ -0,0 +1,188 @@
+//compile with gcc -pthread -o borders_pthread borders_pthread.c
+//run with ./borders_phread
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/time.h>
+#include <pthread.h>
+#include "lib/bmp_structs.h"
+#include "lib/bmp_file.h"
+/**
+*@config DIF When a pixel P>=DIF, pixel is set to black.
+*/
+#define DIF 16
+#define NUM_THREADS 32
+
+pthread_mutex_t lock;
+
+char filename[]="/home/cury/ITESO/Operativos/Practica 3/practica3/img/rocks.bmp";
+
+unsigned char blackandwhite(PIXEL p)
+{
+  return((unsigned char) (0.3*((float)p.red)+0.59*((float)p.green)+0.11*((float)p.blue)));
+}
+
+void processBMP(IMAGE *imagefte, IMAGE *imagedst);
+void *threadProcessBMP(void *threadid);
+
+int main()
+{
+  IMAGE imagefte;//Imagen fuente
+  IMAGE imagedst;//Imagen destino
+
+  //file name
+  char namedest[80];
+  strcpy(namedest,strtok(filename,"."));
+  strcat(filename,".bmp");
+  strcat(namedest,"_P.bmp");
+  printf("Source file: %s\n",filename);
+  printf("Target file: %s\n",namedest);
+
+  //clock
+  clock_t t_inicial=clock();
+  clock_t t_final;
+
+  //load
+  if(loadBMP(filename,&imagefte)==-1)
+  {
+    fprintf(stderr,"ERROR: Couldn't open image\n");
+    exit(1);
+  }
+  printf("\nProcessing image: ROWS= %d, COLUMNS= %d\n\n",imagefte.infoheader.rows,imagefte.infoheader.cols);
+
+  //process
+  processBMP(&imagefte,&imagedst);
+
+  //save
+  puts("guardar");
+  if(saveBMP(namedest,&imagedst)==-1)
+  {
+    fprintf(stderr,"Error al escribir imagen\n");
+    exit(1);
+  }
+
+  //clock result
+  t_final=clock();
+  printf("Tiempo %3.6f segundos\n",((float) t_final- (float)t_inicial)/1000000);
+}
+
+///////////////////////////////////////////////////
+//THREAD IMPLEMENTATION
+
+/**
+*@class thread_info
+*@module structures
+*/
+struct thread_info {
+   int nThread;
+   int fromRow;
+   int status;
+   int toRow;
+   int imageCols;
+   IMAGE *imagefte;//Imagen fuente
+   IMAGE *imagedst;//Imagen destino
+};
+
+/**
+*main thread
+*@method processBMP
+*/
+void processBMP(IMAGE *imagefte, IMAGE *imagedst)
+{
+  memcpy(imagedst,imagefte,sizeof(IMAGE)-sizeof(PIXEL *));
+  int imageRows = imagefte->infoheader.rows;
+  int imageCols = imagefte->infoheader.cols;
+  imagedst->pixel=(PIXEL *)malloc(sizeof(PIXEL)*imageRows*imageCols);
+
+  pthread_t threads[NUM_THREADS];
+  struct thread_info tinfo[NUM_THREADS];
+  int i;
+
+  for(i=0; i<NUM_THREADS; i++)
+  {
+    /*
+    * assign rows to thread
+    */
+    tinfo[i].nThread = i;
+    tinfo[i].status = 1;
+    if (i==0)
+	tinfo[i].fromRow = 1;
+    else
+	tinfo[i].fromRow = i*(imageRows/NUM_THREADS);
+    if((i+1)>=NUM_THREADS)
+      tinfo[i].toRow = imageRows-1;
+    else
+      tinfo[i].toRow = ((i+1)*(imageRows/NUM_THREADS))-1;
+    tinfo[i].imageCols = imageCols;
+    tinfo[i].imagefte = imagefte;
+    tinfo[i].imagedst = imagedst;
+
+    /*
+    * CREATES THE THREAD HERE
+    */
+    int status = pthread_create(&threads[i], NULL, &threadProcessBMP, &tinfo[i]);
+    if (status!=0)
+    {
+      printf("ERROR CODE: %d\n", status);
+      exit(-1);
+    }
+  }
+  //WAIT FOR THREADS
+  for(i=0; i<NUM_THREADS; i++)
+  {	
+    while(tinfo[i].status){
+    }
+  }
+}
+
+void *threadProcessBMP(void *arg)
+{
+    struct thread_info *tinfo = arg;
+    printf("Thread %d working from %d to %d Row\n",tinfo->nThread,tinfo->fromRow,tinfo->toRow);
+
+    int imageCols= tinfo->imageCols;
+    IMAGE *imagefte= tinfo->imagefte;
+    IMAGE *imagedst= tinfo->imagedst;
+    PIXEL *pfte,*pdst;
+    PIXEL *v0,*v1,*v2,*v3,*v4,*v5,*v6,*v7;
+
+    int i,j;
+    for( i=tinfo->fromRow ; i<=tinfo->toRow ; i++)
+      for(j=1;j< imageCols-1;j++)
+        {
+          pfte=imagefte->pixel+imageCols*i+j;
+          v0=pfte-imageCols-1;
+          v1=pfte-imageCols;
+          v2=pfte-imageCols+1;
+          v3=pfte-1;
+          v4=pfte+1;
+          v5=pfte+imageCols-1;
+          v6=pfte+imageCols;
+          v7=pfte+imageCols+1;
+
+          pdst=imagedst->pixel+imageCols*i+j;
+
+          if(abs(blackandwhite(*pfte)-blackandwhite(*v0))>DIF ||
+          abs(blackandwhite(*pfte)-blackandwhite(*v1))>DIF ||
+          abs(blackandwhite(*pfte)-blackandwhite(*v2))>DIF ||
+          abs(blackandwhite(*pfte)-blackandwhite(*v3))>DIF ||
+          abs(blackandwhite(*pfte)-blackandwhite(*v4))>DIF ||
+          abs(blackandwhite(*pfte)-blackandwhite(*v5))>DIF ||
+          abs(blackandwhite(*pfte)-blackandwhite(*v6))>DIF ||
+          abs(blackandwhite(*pfte)-blackandwhite(*v7))>DIF)
+          {
+            pdst->red=0;
+            pdst->green=0;
+            pdst->blue=0;
+          }
+          else
+          {
+            pdst->red=255;
+            pdst->green=255;
+            pdst->blue=255;
+          }
+        }
+	
+	tinfo->status = 0;
+        printf("Thread %d finished\n",tinfo->nThread);
+}
