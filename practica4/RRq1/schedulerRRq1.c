ef0f37c1835d8163a20a1504592c94fa4c44b9b0
CuRy <curyman@gmail.com>
2014-10-11 14:05:17 -0500

    agrega todos los p6333333636

--- practica4/RRq1/schedulerRRq1.c
+++ practica4/RRq1/schedulerRRq1.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <signal.h>
 #include "virtual_processor.h"
- 
+
 extern struct PROCESO proceso[];
 extern struct COLAPROC listos,bloqueados;
 extern int tiempo;
@@ -12,53 +13,74 @@ extern int pars[];
 
 int scheduler(int evento)
 {
-    int cambia_proceso =0;
-    int prox_proceso_a_ejecutar;
+    int cambia_proceso = 0; // bandera de cambio de proceso
+    int prox_proceso_a_ejecutar; // pid del proximo proceso a ejecutar
 
-    prox_proceso_a_ejecutar=pars[1]; // pars[1] = proceso en ejecución
+    prox_proceso_a_ejecutar = pars[1]; // pars[1] = proceso en ejecución
 
-    if(evento==PROCESO_NUEVO)
+    if(evento == PROCESO_NUEVO)
     {
         // Agregar el nuevo proceso a la cola de listos
         // pars[0] es el proceso nuevo
-        proceso[pars[0]].estado=LISTO;
+        proceso[pars[0]].estado = LISTO;
         mete_a_cola(&listos,pars[0]);
         // pars[1] es el proceso en ejecución
         if(tiempo==0) //if (pars[1] == NINGUNO)
-	  cambia_proceso =1;
-    }      
+          cambia_proceso = 1;
+    }
 
-    if(evento==TIMER)
-        printf("Llega interrupcion del Timer\n");
+    if(evento==TIMER) {
+      printf("Llega interrupcion del Timer\n");
 
-                                 
+      // si hay elementos en espera, mete el que esta en ejecucion a la cola
+      if(!cola_vacia(listos))
+      {
+        printf("mi cola NOOO esta vacia");
+      // lo que estaba en ejecucion se mete a listos
+          proceso[pars[1]].estado = LISTO;
+          mete_a_cola(&listos,pars[1]);
+          // y se hace pop
+          cambia_proceso = 1;
+      }
+
+
+    }
+
     if(evento==SOLICITA_E_S)
     {
-        proceso[pars[1]].estado=BLOQUEADO;
+        proceso[pars[1]].estado = BLOQUEADO;
         printf("Solicita E/S Proceso %d\n",pars[1]);
+        // se mete a la cola de bloqueados
+        mete_a_cola(&bloqueados,pars[1]);
+        // y se hace pop
+        cambia_proceso = 1;
     }
     if(evento==TERMINA_E_S)
     {
         // Saber cual proceso terminó E/S
         // pars0 es el proceso desbloqueado
-        proceso[pars[0]].estado=LISTO;
-        prox_proceso_a_ejecutar=pars[0];
+        proceso[pars[0]].estado = LISTO;
+        //prox_proceso_a_ejecutar=pars[0];
         printf("Termina E/S Proceso desbloqueado %d\n",pars[0]);
+        // se saca de la cola de bloqueados y se pone en listos
+        mete_a_cola(&listos,sacar_de_cola(&bloqueados));
+
+
     }
     if(evento==PROCESO_TERMINADO)
     {
         // pars0 = proceso terminado
-        proceso[pars[0]].estado=TERMINADO;
-        cambia_proceso=1; // Indíca que puede poner un proceso nuevo en ejecucion
+        proceso[pars[0]].estado = TERMINADO;
+        cambia_proceso = 1; // Indíca que puede poner un proceso nuevo en ejecucion
     }
-                                        
+
     if(cambia_proceso)
     {
         // Si la cola no esta vacia obtener de la cola el siguiente proceso listo
         if(!cola_vacia(listos))
         {
             prox_proceso_a_ejecutar=sacar_de_cola(&listos);
-	    proceso[prox_proceso_a_ejecutar].estado=EJECUCION;
+            proceso[prox_proceso_a_ejecutar].estado=EJECUCION;
             cambia_proceso=0;
         }
         else
